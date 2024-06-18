472ed98380f3b48926e79490dfe0958cd7050c57
Alim <alimalim77@gmail.com>
2024-06-18 13:30:53 +0530

    Added Slider functionality to Top Albums with equal width for Collapse and Show All

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,18 @@ import CardMusic from "./CardMusic";
 import axios from "axios";
 import Grid from "@mui/material/Grid";
 import { Typography, Button } from "@mui/material";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "../../node_modules/swiper/swiper.min.css";
+import "../../node_modules/swiper/modules/pagination.min.css";
+// 1. Create two useStates for new and top albums
+// 2. use useEffect to fetch the data from WEB API
+// 3. set the data accordingly for both the states using set functions
 const Card = () => {
   const [topAlbumCard, setTopAlbumCard] = useState([]);
   const [newAlbumCard, setNewAlbumCard] = useState([]);
   const [isTopAlbumCollapsed, setIsTopAlbumCollapsed] = useState(true);
   const [isNewAlbumCollapsed, setIsNewAlbumCollapsed] = useState(true);
+  // const [indexes, setIndexes] = useState([1, 6]);
 
   const fetchCardMusic = async (url) => {
     try {
@@ -28,8 +34,8 @@ const Card = () => {
       const newAlbumData = await fetchCardMusic(
         `https://qtify-backend-labs.crio.do/albums/new`
       );
-      setTopAlbumCard(topAlbumData);
-      setNewAlbumCard(newAlbumData);
+      setTopAlbumCard(topAlbumData); //useState that only stores top ALbum - 13/13
+      setNewAlbumCard(newAlbumData); //useState that only stores new ALbum - 15/15
     };
 
     fetchData();
@@ -51,23 +57,57 @@ const Card = () => {
           {isTopAlbumCollapsed ? "Show All" : "Collapse"}
         </Button>
       </div>
-      <Grid container spacing={2} className="albums">
-        {topAlbumCard.map((cardItem, index) => (
-          <Grid
-            item
-            key={cardItem.id}
-            xs={6}
-            sm={4}
-            md={3}
-            lg={2}
-            style={{
-              display: isTopAlbumCollapsed && index > 5 ? "none" : "block",
-            }}
-          >
-            <CardMusic card={cardItem} className="cards" />
+      {isTopAlbumCollapsed ? (
+        <Swiper
+          spaceBetween={15}
+          slidesPerView={6}
+          onSlideChange={() => {
+            console.log("Slide change");
+          }}
+          onSwiper={(swiper) => console.log("Swiper")}
+          className="albums"
+          breakpoints={{
+            320: { slidesPerView: 2 },
+            600: { slidesPerView: 3 },
+            900: { slidesPerView: 4 },
+            1200: { slidesPerView: 6 },
+          }}
+        >
+          <Grid container spacing={2} className="albums">
+            {/* State used for indexing of initial and last 1-6 */}
+            {topAlbumCard.map((cardItem, index) => (
+              <Grid
+                item
+                key={cardItem.id}
+                xs={6}
+                sm={4}
+                md={3}
+                lg={2}
+                style={{
+                  display: isTopAlbumCollapsed && index > 5 ? "none" : "block",
+                }}
+              >
+                <SwiperSlide>
+                  <CardMusic card={cardItem} className="cards" />
+                </SwiperSlide>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </Swiper>
+      ) : (
+        <Grid container spacing={2} className="albums">
+          {/* State used for indexing of initial and last 1-6 */}
+          {topAlbumCard.map((cardItem, index) => (
+            <Grid item key={cardItem.id} xs={6} sm={4} md={3} lg={2}>
+              {/* {
+              ((isTopAlbumCollapsed) && indexes[0] <= index <= indexes[1]) && <CardMusic card={cardItem} className="cards" />
+            } */}
+              <CardMusic card={cardItem} className="cards" />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+
       <div className="albums album-bar">
         <Typography>New Albums</Typography>
         <Button onClick={handleNewAlbumCollapse}>
