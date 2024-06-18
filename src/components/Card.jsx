20b0266382b9627b407c00761cab3bc0941858ef
Alim <alimalim77@gmail.com>
2024-06-18 15:37:24 +0530

    Added swiper functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,11 @@ import CardMusic from "./CardMusic";
 import axios from "axios";
 import Grid from "@mui/material/Grid";
 import { Typography, Button } from "@mui/material";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import "../../node_modules/swiper/swiper.min.css";
 import "../../node_modules/swiper/modules/pagination.min.css";
+
 // 1. Create two useStates for new and top albums
 // 2. use useEffect to fetch the data from WEB API
 // 3. set the data accordingly for both the states using set functions
@@ -114,23 +116,53 @@ const Card = () => {
           {isNewAlbumCollapsed ? "Show All" : "Collapse"}
         </Button>
       </div>
-      <Grid container spacing={2} className="albums">
-        {newAlbumCard.map((cardItem, index) => (
-          <Grid
-            item
-            key={cardItem.id}
-            xs={6}
-            sm={4}
-            md={3}
-            lg={2}
-            style={{
-              display: isNewAlbumCollapsed && index > 5 ? "none" : "block",
-            }}
-          >
-            <CardMusic card={cardItem} className="cards" />
+      {isNewAlbumCollapsed ? (
+        <Swiper
+          spaceBetween={15}
+          slidesPerView={6}
+          onSlideChange={() => {
+            console.log("Slide change");
+          }}
+          onSwiper={(swiper) => console.log("Swiper")}
+          className="albums"
+          breakpoints={{
+            320: { slidesPerView: 2 },
+            600: { slidesPerView: 3 },
+            900: { slidesPerView: 4 },
+            1200: { slidesPerView: 6 },
+          }}
+        >
+          <Grid container spacing={2} className="albums">
+            {/* State used for indexing of initial and last 1-6 */}
+            {newAlbumCard.map((cardItem, index) => (
+              <Grid
+                item
+                key={cardItem.id}
+                xs={6}
+                sm={4}
+                md={3}
+                lg={2}
+                style={{
+                  display: isNewAlbumCollapsed && index > 5 ? "none" : "block",
+                }}
+              >
+                <SwiperSlide>
+                  <CardMusic card={cardItem} className="cards" />
+                </SwiperSlide>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </Swiper>
+      ) : (
+        <Grid container spacing={2} className="albums">
+          {/* State used for indexing of initial and last 1-6 */}
+          {newAlbumCard.map((cardItem, index) => (
+            <Grid item key={cardItem.id} xs={6} sm={4} md={3} lg={2}>
+              <CardMusic card={cardItem} className="cards" />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </>
   );
 };
